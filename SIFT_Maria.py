0cec0b26f17be7bdca6e4d498fbf51399fcccc33
MariaBarrett <Maria@Macintosh.local>
2013-12-09 16:31:03 +0100

    My work so far... It runs, but it's not done. The structure is weird due to arrays. No histogram yet

--- SIFT_Maria.py
+++ SIFT_Maria.py
@@ -7,11 +7,8 @@ import pylab as pl
 from sklearn import cluster
 
 # Extracting test and train set
-#path1 = glob.glob('../VIPExam2/101_ObjectCategories/lobster/*.jpg')
-#path2 = glob.glob('../VIPExam2/101_ObjectCategories/brontosaurus/*.jpg')
-
-path1 = glob.glob('/Users/Maria/Documents/ITandcognition/Github/VIPExam2/101_ObjectCategories/lobster')
-path2 = glob.glob('/Users/Maria/Documents/ITandcognition/Github/VIPExam2/101_ObjectCategories/brontosaurus')
+path1 = glob.glob('../VIPExam2/101_ObjectCategories/lobster/*.jpg')
+path2 = glob.glob('../VIPExam2/101_ObjectCategories/brontosaurus/*.jpg')
 
 train1 = path1[:30]
 train2 = path2[:30]
@@ -28,6 +25,13 @@ def detectcompute(data):
 		kp, des = sift.detectAndCompute(gray,None)
 	return des
 
+def detectcompute2(img):
+	#Same as detectcompute just doesn't loop through a list of images
+	image = cv2.imread(img)
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	kp, des = sift.detectAndCompute(gray,None)
+	return des
+
 
 train_des1 = detectcompute(train1)
 train_des2 = detectcompute(train2)
@@ -37,9 +41,33 @@ X_train = np.concatenate((train_des1, train_des2),axis=0)
 #computing K-Means 
 codebook,distortion = kmeans(whiten(X_train),5)
 idx,distor = vq(X_train,codebook)
-print idx
+#print idx
 
 #--------------------------------------------------------------------------
 #Indexing
 
-def index(data, class):
+def index(data,x):
+#x is the class 
+	indexlist = []
+	temp = []
+	temp2 = []
+	for img in data:
+		#img.split("/")
+		temp.append(x)
+		temp.append(img[-14:]) # filename
+		des = detectcompute2(img) #calling the function that returns descriptors
+		codebook,distortion = kmeans(whiten(des),5)
+		idx,distor = vq(des,codebook)
+		idx.tolist() #doesn't work. It's just ignored. They are still numpy arrays
+		des.tolist()
+		for i in range(len(des)):
+			temp2.append(idx[i])
+			temp2.append(des[i])
+		temp.append(temp2) 
+	indexlist.append(temp)
+	print indexlist[0][0] #print class of first image
+	print indexlist[0]
+
+
+index(train1,2)
+	
