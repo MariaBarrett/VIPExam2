ef2580b42f2eb7018f8a6fecad3ed187c0d66014
MariaBarrett <Maria@Macintosh.local>
2013-12-10 14:35:23 +0100

    writing a table

--- SIFT_Maria.py
+++ SIFT_Maria.py
@@ -44,14 +44,6 @@ def detectcompute(data):
 	return out
 
 
-def detectcompute2(img):
-	#Same as detectcompute just doesn't loop through a list of images
-	image = cv2.imread(img)
-	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-	kp, des = sift.detectAndCompute(gray,None)
-	return des
-
-
 """singledetect(data)
 This function takes a list of image paths as inputs.
 It then outputs each images' path and corresponding SIFT descriptors.
@@ -99,7 +91,7 @@ def bow(images,codebook,clusters):
 	for i in range(len(temp)):
 		out[i].append(temp[i])
 
-	print out #For you Maria, so you can see the structure!
+	#print out #For you Maria, so you can see the structure!
 	return out
 
 
@@ -118,35 +110,21 @@ imtrain = singledetect(train1) #[image1[path,descriptors array].image2[path,desc
 Pdatabase = bow(imtrain,codebook,clu)
 #idx,distor = vq(X_train,codebook)
 
+
 #--------------------------------------------------------------------------
-#Indexing
+#Print in table
 
-def index(data,x):
-#x is the class. This function returns a list of lists of lists. The structure is as follows:
-#[class, filename, [index of first descriptor, [first descriptor], [index of second decriptor [second descriptor]]]]  
-	indexlist = []
-	temp = []
-	temp2 = []
-	for img in data:
-		#img.split("/")
-		temp.append(x)
-		temp.append(img[-14:]) # filename
-		des = detectcompute2(img) #calling the function that returns descriptors
-		codebook,distortion = kmeans(whiten(des),5)
-		idx,distor = vq(des,codebook)
-		idx.tolist() #doesn't work. It's just ignored. They are still numpy arrays
-		des.tolist()
-		for i in range(len(des)):
-			temp2.append(idx[i])
-			temp2.append(des[i])
-		temp.append(temp2)
-		temp2 =[] 
-	indexlist.append(temp)
-	temp=[]
-	#print indexlist[0][0] #print class of first image
-	#print indexlist[0]
+htmltable = open("table.htm","r+") 
+
+begin = "<htm><body><table cellpadding=5><tr><th>Filename</th><th>Histogram</th></tr>"
+htmltable.write(begin)
+
+for i in range(len(Pdatabase)):
+    middle = "<tr><td>%(filename)s</td><td>%(histogram)s</td></tr>" % {"filename": Pdatabase[i][0], "histogram": Pdatabase[i][-1]}
+    htmltable.write(middle)
+
+end = "</table></body></html>"    
+htmltable.write(end)
 
 
-index(train1,2)
-	
-
+htmltable.close() 
