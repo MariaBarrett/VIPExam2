9b22932193160a30e9c2073a17c7e1a496aa0b0a
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2013-12-14 18:44:14 +0100

    Merge cus I have to.

--- SIFT_Maria.py
+++ SIFT_Maria.py
@@ -30,7 +30,7 @@ test2 = path2
 test1.extend(test2)
 # Defining classifiers as variables and other useful variables
 sift = cv2.SIFT()
-k = 500
+k = 1000
 
 #--------------------------------------------------------------------------
 #Detection and bag of visual words
